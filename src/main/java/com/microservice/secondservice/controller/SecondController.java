3303450a2ada7460f7f4f9326bb9ef793d81bf22
parmuthu <parmuthu2002@gmail.com>
2024-04-21 23:00:32 +0530

    new repo created for commit

---
+++ src/main/java/com/microservice/secondservice/controller/SecondController.java
@@ -0,0 +1,15 @@
+package com.microservice.secondservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consumer")
+public class SecondController {
+
+	@GetMapping("/message")
+	public String test() {
+		return "Hello from Second Service";
+	}
+}
