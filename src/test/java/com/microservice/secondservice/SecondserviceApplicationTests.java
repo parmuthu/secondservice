95197741bc6d9893e68efcf0e7ceb4df1fe78509
parmuthu <parmuthu2002@gmail.com>
2024-04-28 22:57:50 +0530

    docker files added

--- src/test/java/com/microservice/secondservice/SecondserviceApplicationTests.java
+++ src/test/java/com/microservice/secondservice/SecondserviceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.microservice.secondservice;
+/*package com.microservice.secondservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=SecondserviceApplicationTests.class)
 class SecondserviceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
-}
+}*/
